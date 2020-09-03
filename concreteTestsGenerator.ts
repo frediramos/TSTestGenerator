73bf8df2d5b6b5784ec0d0065286970e7c82e59f
vdr07 <valentimpro07@gmail.com>
2020-09-03 19:46:35 +0100

    Generator of tests with concrete values

---
+++ concreteTestsGenerator.ts
@@ -0,0 +1,23 @@
+import * as ts from "typescript";
+import * as fs from "fs";
+
+var test_path = process.argv.slice(2)[0];
+var json_path = "./"+test_path.split("/")[0]+"/failing_models/"+test_path.split("/")[1].split(".")[0]+".json";
+var json = require(json_path);
+
+for(var i = 0; i<json.models.length;i++){
+    var test_code = fs.readFileSync(test_path, 'utf8');
+
+    Object.keys(json.models[i]).forEach(function (var_name) { 
+        
+        test_code = test_code.split(`symb_number(${var_name})`).join(json.models[i][var_name]);
+        test_code = test_code.split(`symb_string(${var_name})`).join(json.models[i][var_name]);
+
+    });
+    fs.writeFileSync(test_path.split("/")[0]+"/concrete_tests/concrete_"+test_path.split("/")[1].split(".")[0]+(i+1)+".js",test_code);
+
+}
+
+
+
+
