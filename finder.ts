b79070dac6d73f9f7b970eb414f4bb60f680f564
vdr07 <valentimpro07@gmail.com>
2020-09-24 17:50:04 +0100

    Preparations for adding interface type

--- finder.ts
+++ finder.ts
@@ -1,5 +1,4 @@
 import * as ts from "typescript";
-import * as fs from "fs";
 
 
 //Class to store parameters type, return type and class type if it is a class method and not a function
@@ -10,7 +9,7 @@ export class ComposedInfo {
 
 
 //Interface of a simple Hashtable
-export interface HashTable<T>{
+export interface HashTable<T> {
     [key:string] : T;
 }
 
@@ -21,17 +20,18 @@ class ClassVertex{
 
 
 //Class that will store all the information in the program
-export class ProgramInfo{
+export class ProgramInfo {
     Checker: ts.TypeChecker;
     ClassesInfo: HashTable<ts.Type> = {};
     MethodsInfo: HashTable<HashTable<ComposedInfo>> = {};
     ConstructorsInfo: HashTable<ComposedInfo []> = {};
     PropertiesInfo: HashTable<HashTable<ts.Type>> = {};
     FunctionsInfo: HashTable<ComposedInfo> = {};
-    cycles_hash:HashTable<string[][]> = {};
+    InterfacesInfo: HashTable<ts.Type> = {};
+    cycles_hash : HashTable<string[][]> = {};
 
 
-    hasClass(class_name:string):boolean{
+    hasClass(class_name:string):boolean {
         if(this.ClassesInfo[class_name]===undefined)
             return false;
         return true;
@@ -39,12 +39,12 @@ export class ProgramInfo{
 }
 
 //::::::::Function to set up for the traversation of the AST::::::::
-export function finder(fileNames:string[]):ProgramInfo{
+export function finder(fileNames:string[]):ProgramInfo {
 
-    var prog_info=new ProgramInfo();
+    var prog_info = new ProgramInfo();
     const program = ts.createProgram(fileNames, { target: ts.ScriptTarget.ES5, module: ts.ModuleKind.CommonJS});
     const checker = program.getTypeChecker();
-    prog_info.Checker=checker;
+    prog_info.Checker = checker;
     const visitASTWithChecker = visitAST.bind(undefined, checker,prog_info);
 
     for (const sourceFile of program.getSourceFiles()) 
@@ -79,16 +79,16 @@ function visitAST(checker: ts.TypeChecker,  prog_info:ProgramInfo, node: ts.Node
             const member_symbol = checker.getSymbolAtLocation(member.name);
 
             //-----Constructors handling-----
-            if(ts.isConstructorDeclaration(member)){
+            if(ts.isConstructorDeclaration(member)) {
                 
                 //Must initialize sub-array, otherwise it is undefined
                 if(prog_info.ConstructorsInfo[symbol.getName()]===undefined)
                     prog_info.ConstructorsInfo[symbol.getName()] = [];
                     
-                prog_info.ConstructorsInfo[symbol.getName()][constructor_count]=new ComposedInfo();
+                prog_info.ConstructorsInfo[symbol.getName()][constructor_count] = new ComposedInfo();
 
                 //Store the types of the parameters in arg_types in the position "<ClassName><ConstructorNumber>" of ConstructorsInfo 
-                for(const parameter of member.parameters){
+                for(const parameter of member.parameters) {
                     const symbol_p = checker.getSymbolAtLocation(parameter.name);
                     prog_info.ConstructorsInfo[symbol.getName()][constructor_count].arg_types.push(checker.getTypeOfSymbolAtLocation(symbol_p, symbol_p.valueDeclaration!));
                 }
@@ -100,22 +100,22 @@ function visitAST(checker: ts.TypeChecker,  prog_info:ProgramInfo, node: ts.Node
             }
 
             //-----Methods handling-----
-            else if (ts.isMethodDeclaration(member)){
+            else if (ts.isMethodDeclaration(member)) {
 
                 //Must initialize sub-hashtable, otherwise it is undefined
                 if(prog_info.MethodsInfo[symbol.getName()]===undefined)
                     prog_info.MethodsInfo[symbol.getName()] = {};
 
-                prog_info.MethodsInfo[symbol.getName()][member_symbol.getName()]=new ComposedInfo();
+                prog_info.MethodsInfo[symbol.getName()][member_symbol.getName()] = new ComposedInfo();
 
                 //Store the types of the parameters in arg_types in the position "<ClassName><MethodName>" of MethodsInfo 
-                for(const parameter of member.parameters){
+                for(const parameter of member.parameters) {
                     const symbol_p = checker.getSymbolAtLocation(parameter.name);
                     prog_info.MethodsInfo[symbol.getName()][member_symbol.getName()].arg_types.push(checker.getTypeOfSymbolAtLocation(symbol_p, symbol_p.valueDeclaration!));
                 }
 
                 //Store the return type of the method in ret_type in the position "<ClassName><MethodName>" of MethodsInfo 
-                prog_info.MethodsInfo[symbol.getName()][member_symbol.getName()].ret_type=checker.getTypeAtLocation(member.type);
+                prog_info.MethodsInfo[symbol.getName()][member_symbol.getName()].ret_type = checker.getTypeAtLocation(member.type);
             }
 
             //-----Properties handling-----
@@ -123,10 +123,10 @@ function visitAST(checker: ts.TypeChecker,  prog_info:ProgramInfo, node: ts.Node
 
                 //Must initialize sub-hashtable, otherwise it is undefined
                 if(prog_info.PropertiesInfo[symbol.getName()]===undefined)
-                    prog_info.PropertiesInfo[symbol.getName()]={};
+                    prog_info.PropertiesInfo[symbol.getName()] = {};
 
                 //Stores the property type in the position"<ClassName><PropertyName>" of PropertiesInfo
-                prog_info.PropertiesInfo[symbol.getName()][member_symbol.getName()]=checker.getTypeAtLocation(member.type);
+                prog_info.PropertiesInfo[symbol.getName()][member_symbol.getName()] = checker.getTypeAtLocation(member.type);
             }
         }
     }
@@ -138,18 +138,64 @@ function visitAST(checker: ts.TypeChecker,  prog_info:ProgramInfo, node: ts.Node
         prog_info.FunctionsInfo[symbol.getName()]=new ComposedInfo();
         const functionType = checker.getTypeOfSymbolAtLocation(symbol,symbol.valueDeclaration!);
 
-        for(const signature of functionType.getCallSignatures()){
+        for(const signature of functionType.getCallSignatures()) {
 
             //Store the types of the parameters in arg_types in the position "<FunctionName>" of FunctionsInfo 
-            for(const parameter of signature.parameters){
+            for(const parameter of signature.parameters) {
                 prog_info.FunctionsInfo[symbol.getName()].arg_types.push(checker.getTypeOfSymbolAtLocation(parameter, parameter.valueDeclaration!));
             }
             
             //Store the return type of the function in ret_type in the position "<FunctionName>" of FunctionsInfo
-            prog_info.FunctionsInfo[symbol.getName()].ret_type=signature.getReturnType();
+            prog_info.FunctionsInfo[symbol.getName()].ret_type = signature.getReturnType();
         }
     }
 
+    //-----Interfaces handling-----
+    else if(ts.isInterfaceDeclaration(node) && node.name) {
+
+        const symbol = checker.getSymbolAtLocation(node.name);
+
+        prog_info.InterfacesInfo[symbol.getName()] = checker.getTypeAtLocation(node);
+        
+        for(const member of node.members) {
+
+            const member_symbol = checker.getSymbolAtLocation(member.name);
+
+            //-----Methods handling-----
+            if (ts.isMethodDeclaration(member)) {
+                console.log(symbol.getName()+"'s method : "+member_symbol.getName());
+                /*
+                //Must initialize sub-hashtable, otherwise it is undefined
+                if(prog_info.MethodsInfo[symbol.getName()]===undefined)
+                    prog_info.MethodsInfo[symbol.getName()] = {};
+
+                prog_info.MethodsInfo[symbol.getName()][member_symbol.getName()] = new ComposedInfo();
+
+                //Store the types of the parameters in arg_types in the position "<ClassName><MethodName>" of MethodsInfo 
+                for(const parameter of member.parameters) {
+                    const symbol_p = checker.getSymbolAtLocation(parameter.name);
+                    prog_info.MethodsInfo[symbol.getName()][member_symbol.getName()].arg_types.push(checker.getTypeOfSymbolAtLocation(symbol_p, symbol_p.valueDeclaration!));
+                }
+
+                //Store the return type of the method in ret_type in the position "<ClassName><MethodName>" of MethodsInfo 
+                prog_info.MethodsInfo[symbol.getName()][member_symbol.getName()].ret_type = checker.getTypeAtLocation(member.type);
+                */
+            }
+
+            //-----Properties handling-----
+            else if(ts.isPropertyDeclaration(member)){
+                console.log(symbol.getName()+"'s property : "+member_symbol.getName());
+                /*
+                //Must initialize sub-hashtable, otherwise it is undefined
+                if(prog_info.PropertiesInfo[symbol.getName()]===undefined)
+                    prog_info.PropertiesInfo[symbol.getName()] = {};
+
+                //Stores the property type in the position"<ClassName><PropertyName>" of PropertiesInfo
+                prog_info.PropertiesInfo[symbol.getName()][member_symbol.getName()] = checker.getTypeAtLocation(member.type);
+                */
+            }
+        }
+    }
 }
 
 
@@ -163,25 +209,23 @@ function isNodeExported(node: ts.Node): boolean {
 
 
 ////::::::::This function initializes the classes graph::::::::
-function initializeClassesGraph(classes_graph:HashTable<ClassVertex>,program_info:ProgramInfo){
+function initializeClassesGraph(classes_graph:HashTable<ClassVertex>,program_info:ProgramInfo) {
 
     Object.keys(program_info.ConstructorsInfo).forEach(function (class_name) { 
         
-        if(classes_graph[class_name]===undefined){
-            classes_graph[class_name]=new ClassVertex();
-            classes_graph[class_name].edges=[];
-            classes_graph[class_name].visited=0;
+        if(classes_graph[class_name]===undefined) {
+            classes_graph[class_name] = new ClassVertex();
+            classes_graph[class_name].edges = [];
+            classes_graph[class_name].visited = 0;
         }
 
-        for(var i=0; i<program_info.ConstructorsInfo[class_name].length; i++){
-            for (var j=0; j<program_info.ConstructorsInfo[class_name][i].arg_types.length; j++){
+        for(var i = 0; i<program_info.ConstructorsInfo[class_name].length; i++) {
+            for (var j = 0; j<program_info.ConstructorsInfo[class_name][i].arg_types.length; j++) {
                 
                 var arg_type = program_info.ConstructorsInfo[class_name][i].arg_types[j];
                 var arg_type_str = program_info.Checker.typeToString(arg_type);
                 
-                if(arg_type_str !=="string" && arg_type_str !=="number"){
-
-                        
+                if(arg_type_str !=="string" && arg_type_str !=="number") {
                     classes_graph[class_name].edges.push(arg_type_str);
                 }
             }
@@ -191,14 +235,14 @@ function initializeClassesGraph(classes_graph:HashTable<ClassVertex>,program_inf
 
 
 ////::::::::This function visits the classes graph::::::::
-function visitGraph(graph:HashTable<ClassVertex>,curr_path:string[],cycles:string[][],cycles_hash:HashTable<string[][]>){
+function visitGraph(graph:HashTable<ClassVertex>,curr_path:string[],cycles:string[][],cycles_hash:HashTable<string[][]>) {
     var vertex = graph[curr_path[curr_path.length-1]];
 
-    for(var i=0;i<vertex.edges.length;i++){
+    for(var i = 0;i<vertex.edges.length;i++){
         
-        if(graph[vertex.edges[i]]!==undefined && graph[vertex.edges[i]].visited===1){
+        if(graph[vertex.edges[i]]!==undefined && graph[vertex.edges[i]].visited===1) {
             cycles.push(curr_path);
-            for(var i=0;i<curr_path.length;i++){
+            for(var i = 0;i<curr_path.length;i++) {
 
                 if(cycles_hash[curr_path[i]]===undefined)
                     cycles_hash[curr_path[i]] = [];
@@ -207,32 +251,32 @@ function visitGraph(graph:HashTable<ClassVertex>,curr_path:string[],cycles:strin
             }
         }
 
-        else if(graph[vertex.edges[i]]!==undefined && graph[vertex.edges[i]].visited===0){
+        else if(graph[vertex.edges[i]]!==undefined && graph[vertex.edges[i]].visited===0) {
             curr_path.push(vertex.edges[i]);
-            graph[vertex.edges[i]].visited=1;
+            graph[vertex.edges[i]].visited = 1;
             visitGraph(graph,curr_path,cycles,cycles_hash);
         }
     }
-    graph[curr_path[curr_path.length-1]].visited=2;
+    graph[curr_path[curr_path.length-1]].visited = 2;
 }
 
 
 //::::::::Returns the method and properties that will generate a cycle while generating the tests::::::::
-export function findCycles(program_info:ProgramInfo){
+export function findCycles(program_info:ProgramInfo) {
     var classes_graph:HashTable<ClassVertex> = {};
-    var cycles:string[][]=[];
+    var cycles:string[][] = [];
 
     initializeClassesGraph(classes_graph,program_info);
 
     Object.keys(classes_graph).forEach(function (class_name) {
 
-        if(classes_graph[class_name].visited!==2){
+        if(classes_graph[class_name].visited!==2) {
 
-            var curr_path =[];
+            var curr_path = [];
             curr_path.push(class_name);
-            classes_graph[class_name].visited=1;
+            classes_graph[class_name].visited = 1;
             visitGraph(classes_graph,curr_path,cycles,program_info.cycles_hash);
-            classes_graph[class_name].visited=2;
+            classes_graph[class_name].visited = 2;
         }
     });
 
